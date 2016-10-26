36357ea692a21c86b71f5976ad64779eeaea965e
Linghao Zhang <zlhdnc1994@gmail.com>
2016-10-26 20:47:29 +0800

    boilerplate codes for actual application

---
+++ src/MiniJavaBaseListener.java
@@ -0,0 +1,146 @@
+// Generated from MiniJava.g4 by ANTLR 4.5.3
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link MiniJavaListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class MiniJavaBaseListener implements MiniJavaListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterGoal(MiniJavaParser.GoalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitGoal(MiniJavaParser.GoalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMainClass(MiniJavaParser.MainClassContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMainClass(MiniJavaParser.MainClassContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVarDeclaration(MiniJavaParser.VarDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVarDeclaration(MiniJavaParser.VarDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterType(MiniJavaParser.TypeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitType(MiniJavaParser.TypeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement(MiniJavaParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement(MiniJavaParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression(MiniJavaParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression(MiniJavaParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIdentifier(MiniJavaParser.IdentifierContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIdentifier(MiniJavaParser.IdentifierContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ src/MiniJavaListener.java
@@ -0,0 +1,99 @@
+// Generated from MiniJava.g4 by ANTLR 4.5.3
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link MiniJavaParser}.
+ */
+public interface MiniJavaListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#goal}.
+	 * @param ctx the parse tree
+	 */
+	void enterGoal(MiniJavaParser.GoalContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#goal}.
+	 * @param ctx the parse tree
+	 */
+	void exitGoal(MiniJavaParser.GoalContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#mainClass}.
+	 * @param ctx the parse tree
+	 */
+	void enterMainClass(MiniJavaParser.MainClassContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#mainClass}.
+	 * @param ctx the parse tree
+	 */
+	void exitMainClass(MiniJavaParser.MainClassContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#classDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void enterClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#classDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void exitClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#varDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void enterVarDeclaration(MiniJavaParser.VarDeclarationContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#varDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void exitVarDeclaration(MiniJavaParser.VarDeclarationContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#methodDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void enterMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#methodDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void exitMethodDeclaration(MiniJavaParser.MethodDeclarationContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void enterType(MiniJavaParser.TypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void exitType(MiniJavaParser.TypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement(MiniJavaParser.StatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement(MiniJavaParser.StatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpression(MiniJavaParser.ExpressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpression(MiniJavaParser.ExpressionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MiniJavaParser#identifier}.
+	 * @param ctx the parse tree
+	 */
+	void enterIdentifier(MiniJavaParser.IdentifierContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MiniJavaParser#identifier}.
+	 * @param ctx the parse tree
+	 */
+	void exitIdentifier(MiniJavaParser.IdentifierContext ctx);
+}
