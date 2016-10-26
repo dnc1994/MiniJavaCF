723590951f9c00076f8cfffd03e9779019c7362d
Linghao Zhang <zlhdnc1994@gmail.com>
2016-10-26 21:54:10 +0800

    boilerplate codes for underlining errors

--- src/SyntaxErrorListener.java
+++ src/SyntaxErrorListener.java
@@ -17,11 +17,32 @@ public class SyntaxErrorListener extends BaseErrorListener {
                             String msg,
                             RecognitionException e)
     {
-        List<String> stack = ((Parser)recognizer).getRuleInvocationStack();
-        Collections.reverse(stack);
-        System.err.println("rule stack: "+stack);
-        System.err.println("line "+line+":"+charPositionInLine+" at "+
-                           offendingSymbol+": "+msg);
+        // List<String> stack = ((Parser)recognizer).getRuleInvocationStack();
+        // Collections.reverse(stack);
+        // System.err.println("rule stack: "+stack);
+        // System.err.println("line "+line+":"+charPositionInLine+" at "+
+        //                    offendingSymbol+": "+msg);
+
+        System.err.println("syntax error at line " + line + ":" + charPositionInLine + " -> " + msg);
+        underlineError(recognizer, (Token)offendingSymbol, line, charPositionInLine);
+        System.err.println();
+    }
+
+    protected void underlineError(Recognizer recognizer, Token offendingToken,
+                                  int line, int charPositionInLine) {
+        CommonTokenStream tokens = (CommonTokenStream)recognizer.getInputStream();
+        String input = tokens.getTokenSource().getInputStream().toString();
+        String[] lines = input.split("\n");
+        String errorLine = lines[line - 1];
+
+        System.err.println(errorLine);
+        for (int i = 0; i < charPositionInLine; i ++) System.err.print(" ");
+        int start = offendingToken.getStartIndex();
+        int stop = offendingToken.getStopIndex();
+        if (start >= 0 && stop >= 0) {
+            for (int i = start; i <= stop; i ++) System.err.print("^");
+        }
+
     }
 
 }
