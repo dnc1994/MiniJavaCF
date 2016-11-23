3b7447ee30e58c4d05b9e7651621bad6233c00f7
Linghao Zhang <zlhdnc1994@gmail.com>
2016-11-24 03:43:36 +0800

    add type attr for symbol

--- src/Symbol.java
+++ src/Symbol.java
@@ -1,8 +1,14 @@
 public class Symbol {
     String name;
+    String type;
 
     public Symbol(String name) {
         this.name = name;
+    }
+
+    public Symbol(String name, String type) {
+        this(name);
+        this.type = type;
     }
 
     public String getName() {
