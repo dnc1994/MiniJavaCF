446f3ecf8e5cec49dca3eaac7635d6416d6da7e4
Linghao Zhang <zlhdnc1994@gmail.com>
2016-11-24 02:47:43 +0800

    adpat method class

--- src/Method.java
+++ src/Method.java
@@ -1,10 +1,36 @@
-public class Method {
+import java.utils.*;
+
+public class Method extends Symbol implements Scope {
     private String name;
     private String type;
+    private Scope parent;
 
-    public Method(String name, String type) {
+    private Map<String, Map> locals = new Map<String, Map>();
+
+    public Method(String name, String type, Scope parent) {
         this.name = name;
         this.type = type;
+        this.parent = parent;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Scope getParentScope() {
+        return this.parent;
+    }
+
+    @Override
+    public defineSymbol(Symbol symbol) {
+
+    }
+
+    @Override
+    public lookupSymbol(String name) {
+
     }
 
 }
