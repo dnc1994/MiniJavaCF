1a964933d4902079e31d3fe9438418a6cb3d10bf
Linghao Zhang <zlhdnc1994@gmail.com>
2016-11-24 06:56:57 +0800

    add testbed for 2nd pass

---
+++ test/2ndPassTestbed.java
@@ -0,0 +1,28 @@
+class Testbed {
+    public static void main(String[] x){
+        System.out.println(new Foo().Excite(5));
+    }
+}
+
+class Foo {
+    public int Excite(int x) {
+        int y;
+        Bar z;
+        if (x < 1)
+            y = 1;
+          else 
+            y = x * (this.Excite(x-1));
+        return y;
+    }
+
+    public Bar ExciteX(int x) {
+        return 0;
+    }
+
+}
+
+class Biu extends Cha {
+    public int ExciteY(int x) {
+        return 0;
+    }
+}
