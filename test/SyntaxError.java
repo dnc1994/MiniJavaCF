b78176019b4ca3042b51ca450cc774a857163acc
Linghao Zhang <zlhdnc1994@gmail.com>
2016-11-26 01:36:30 +0800

    add test for syntax error reporting

---
+++ test/SyntaxError.java
@@ -0,0 +1,16 @@
+class Testbed {
+    public static void main(String[] x){
+        System.out.println(new Foo().Excite(5, 6));
+    }
+}
+
+class Foo {
+    public # Excite(int x, int z) {
+        if (x < 1)
+            y = 1;
+        else 
+            y = x * (this.Excite(x-1));
+        return x + y + z;
+    }
+
+}
