a30f8a678619d85297caf55b745f3bfa27b7e0b5
Linghao Zhang <zlhdnc1994@gmail.com>
2016-11-28 23:37:40 +0800

    submit script

--- submit.sh
+++ submit.sh
@@ -1,7 +1,13 @@
+pandoc doc/report.md --latex-engine=xelatex -o doc/report.pdf
 git log > doc/git.log
 rm -rf submission
 mkdir submission
 cp README.md submission/
+cp doc/report.pdf submission/
 cp -r doc submission/
 cp -r src submission/
+rm submission/src/*.class
 cp -r test submission/
+cp -r submission 13307130225/
+zip -r 13307130225.zip 13307130225/
+rm -rf 13307130225
