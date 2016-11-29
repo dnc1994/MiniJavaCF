90dce4d3d2fcadf8fa97ed63da7e81858a9430a3
Linghao Zhang <zlhdnc1994@gmail.com>
2016-11-30 05:11:20 +0800

    chore

--- submit.sh
+++ submit.sh
@@ -1,12 +1,13 @@
-pandoc doc/report.md --latex-engine=xelatex -o doc/report.pdf
+# pandoc doc/report.md --latex-engine=xelatex -o doc/report.pdf
 git log > doc/git.log
-rm -rf submission
-mkdir submission
-cp README.md submission/
-cp doc/report.pdf submission/
-cp -r doc submission/
-cp -r src submission/
-rm submission/src/*.class
-cp -r test submission/
-zip -r 13307130225.zip submission/
-rm -rf submission
+rm -rf 13307130225
+mkdir 13307130225
+cp README.md 13307130225/
+cp doc/report.pdf 13307130225/
+cp -r doc 13307130225/
+cp -r src 13307130225/
+cp -r test 13307130225/
+rm 13307130225/src/*.class
+rm -rf 13307130225/doc/img
+zip -r 13307130225.zip 13307130225/
+rm -rf 13307130225
